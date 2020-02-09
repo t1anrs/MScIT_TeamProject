d0b81934c3352214ca56680cb637b75c8bfe6c80
t1anrs <60039777+t1anrs@users.noreply.github.com>
2020-02-09 05:09:35 +0000

    Update Card.java

--- src/game/Card.java
+++ src/game/Card.java
@@ -4,47 +4,50 @@
  *
  */
 public class Card {
-	private String title, infoText;
-	private final int NrOfAttributes=5;
-	private int[] attribute= new int[NrOfAttributes];
 	
-	/**constructor
-	 * @param String containing card info
-	 * */
-	public Card(String infor) {	  //takes a string and breaks it into chunks
-		this.infoText = infor;
-    	String [] info = infoText.split("\\s+");
-    	this.title = info[0];					    	// title is the first word in the String
-    	for (int i = 0; i < NrOfAttributes; i++){		// put category keywords into attribute array
-    		attribute[i] = Integer.parseInt(info[i+1]);
+	//Instance variables
+	private String cardName, cardInfo;
+	private final int sumOfAttributes=5;
+	private int[] attribute= new int[sumOfAttributes];
+	
+	/**
+	 * card constructor
+	 * @param String  card information
+	 * 
+	 */
+	public Card(String cardInformation) {	  //takes a string and breaks it into chunks
+		this.cardInfo = cardInformation;
+    	String [] categories = cardInformation.split("\\s+");
+    	this.cardName = categories[0];					    	// title is the first word in the String
+    	for (int i = 0; i < sumOfAttributes; i++){		// put category keywords into attribute array
+    		attribute[i] = Integer.parseInt(categories[i+1]);
     	}
 	}
 	
 
 	/**
-	 * returns a String formatted for being displayed on the screen
-	 * includes the title and value of card attributes
-	 * */
+	 * @return a String formatted ----- the card name and attributes value  	 * 
+	 */
 	public String toString(){	
-		return infoText;  // return String used in constructor
+		return cardInfo;  // return String used in constructor
 	}
 	
 	
 	/**
-	 * returns attribute value.
-	 * @param int [1..5] marks which attribute value
-	 * is requested
-	 * */
-	public int getCategoryValue(int categoryIn){
-		return attribute[categoryIn-1];
+	 * get attribute value.
+	 * @param integer represent category
+	 * @return the attribute value of the category
+	 */
+	public int getCategoryValue(int categoryNum){
+		return attribute[categoryNum-1];
 	}
 	
 	
 	/**
-	 * @return card title
-	 * */
-	public String getTitle(){
-		return title;
+	 * @return card name
+	 */
+	public String getcardName(){
+		return cardName;
 		}
 	
 }
