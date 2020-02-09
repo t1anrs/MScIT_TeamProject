07ad4584a4a81f0b0b600925c174deccbfae7b5c
t1anrs <60039777+t1anrs@users.noreply.github.com>
2020-02-09 05:10:41 +0000

    Update Player.java

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,51 +1,57 @@
 /** 
-	 * This class model a player,
-	 * indicates whether the player is human or not, 
-	 * selects the card category for game-play and increments the number of games won. 
+	 * player class
+	 * choose the card category for computer players
 	 * @author RUNSHENG TIAN
 	 * 
 	 */
 public class Player {
 	
 	
-	//Instance variables
+	    //Instance variables
 		private final int numOfCategories;
-		private Deck playerHand;
-		private int playerNumber;
+		private Decklist cardsInPlayerHand;
+		private int playerNumber,categoryNum;
 		private int roundsWon;
 
-		public Player(int playerNumber, Deck playerHand) {
+		public Player(int playerNumber, Decklist cardsInPlayerHand) {
 			this.playerNumber = playerNumber;
-			this.playerHand = playerHand;
-			numOfCategories = playerHand.getCategories().length;
+			this.cardsInPlayerHand = cardsInPlayerHand;
+			numOfCategories = cardsInPlayerHand.getCategory().length;
 		}
 
 		/**
-		 * Method selects highest attribute for computer player and returns its category index.
-		 * @return index of the category chosen
+		 * choose the highest attribute value of category for computer player
+		 * @return index of the chosen category 
 		 */
 		public int chooseCategory() {  
-			Card c = playerHand.seeTopCard();
+			Card c = cardsInPlayerHand.showTheTopCard();
 			
-			int index = 1;
+		    categoryNum = 1;
 			for(int max = 0, i = 0; i < numOfCategories; i++) {
-				int categoryValue = c.getCategoryValue(i + 1);			// Categories count from 1
+				int categoryValue = c.getCategoryValue(i + 1);			
 				if(categoryValue > max) {
 					max = categoryValue;
-					index = i + 1;			// Categories count from 1
+					categoryNum = i + 1;			
 				}
 			}
-			return index;
+			return categoryNum;
 		}
 		
 		/**
-		 * Method transfers card from this player's deck to
-		 * other players' decks/communal deck upon rounds being completed.
+		 * transfers card from this player's deck to other players' decks or communal deck
 		 */ 
-		public void transferCardTo(Deck deck) {   
-			deck.addCardToBottom(playerHand.getTopCard());
+		public void transferCardTo(Decklist deck) {   
+			deck.addCardToLast(cardsInPlayerHand.getTheTopCard());
 		}
-
+        
+		public Decklist getDeck(){   
+			return cardsInPlayerHand;
+		}
+		
+		public int getPlayerNumber() {
+			return playerNumber;
+		}
+		
 		/**
 		 * Method increments rounds won.
 		 */
@@ -57,12 +63,6 @@ public class Player {
 			return roundsWon;
 		}
 
-		public Deck getDeck(){   
-			return playerHand;
-		}
 		
-		public int getPlayerNumber() {
-			return playerNumber;
-		}
 
 }
